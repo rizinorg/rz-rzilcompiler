f8f64134d680157b6d1242484b09f3a21aaef801
Rot127 <45763064+Rot127@users.noreply.github.com>
2022-05-18 19:00:29 +0000

    Add quics QEMU shortcode and shortcode grammar. (#4)
    * Add quics QEMU shortcode and shortcode grammar.
    
    * Update Transformer test to use shortcode grammar
    
    * Delete unused files.
    
    * Add Preprocessor class.

--- Preprocessor/notes.md
+++
@@ -1,2 +0,0 @@
-ZXTN macro in `decode.c`
-#define fZXTN(N, M, VAL) ((VAL) & ((1LL << (N)) - 1))
--- Tests/TestTransformer.py
+++ Tests/TestTransformer.py
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: LGPL-3.0-only
 
 from Transformer.ManualTransformer import ManualTransformer
-from testcases import transform_test
+from Tests.testcases import transform_test
 from lark import Lark
 
 
 class TestTransformer:
 
     def test_transformer(self):
-        with open("../Parser/Grammars/Hexagon/ProgRefManual_Grammar.lark") as f:
+        with open("../Parser/Grammars/Hexagon/shortcode.lark") as f:
             grammar = "".join(f.readlines())
         parser = Lark(grammar, start="fbody")
         for beh in transform_test:
