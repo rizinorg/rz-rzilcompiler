2e155c818d397973a4dc9f781920d0bdcedf5a78
Rot127 <unisono@quyllur.org>
2023-10-02 13:27:27 -0500

    Tests: return the result string, not just the exception.

--- Tests/TestTransformer.py
+++ Tests/TestTransformer.py
@@ -127,12 +127,12 @@ class TestTransforming(unittest.TestCase):
 
     def compile_behavior(
         self, behavior: str, transformer: RZILTransformer = None
-    ) -> Exception:
+    ) -> Exception | str:
         exception = None
         try:
             tree = self.parser.parse(behavior)
             if transformer:
-                transformer.transform(tree)
+                return transformer.transform(tree)
             else:
                 transformer = RZILTransformer(ArchEnum.HEXAGON, sub_routines=self.compiler.sub_routines,
                         parameters=[Parameter("pkt", get_value_type_by_c_type("HexPkt")),
@@ -141,7 +141,7 @@ class TestTransforming(unittest.TestCase):
                                     ],
                         return_type=ValueType(False, 32, VTGroup.EXTERNAL, "RzILOpEffect"),
                         )
-                transformer.transform(tree)
+                return transformer.transform(tree)
         except UnexpectedToken as e:
             # Parser got unexpected token
             exception = e
@@ -173,247 +173,247 @@ class TestTransforming(unittest.TestCase):
 
     def test_J2_jump(self):
         behavior = self.insn_behavior["J2_jump"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_jumpr(self):
         behavior = self.insn_behavior["J2_jumpr"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_jumpt(self):
         behavior = self.insn_behavior["J2_jumpt"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_jumpf(self):
         behavior = self.insn_behavior["J2_jumpf"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_jumprt(self):
         behavior = self.insn_behavior["J2_jumprt"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_jumprf(self):
         behavior = self.insn_behavior["J2_jumprf"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J4_cmpgti_tp0_jump_t(self):
         behavior = self.insn_behavior["J4_cmpgti_tp0_jump_t"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
         behavior = self.insn_behavior["J4_cmpgti_tp0_jump_t"][1]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_call(self):
         behavior = self.insn_behavior["J2_call"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_J2_loop0r(self):
         behavior = self.insn_behavior["J2_loop0r"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_L2_loadrd_io(self):
         behavior = self.insn_behavior["L2_loadrd_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_S2_storeri_io(self):
         behavior = self.insn_behavior["S2_storeri_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_S2_storerd_io(self):
         behavior = self.insn_behavior["S2_storerd_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_L4_return(self):
         behavior = self.insn_behavior["L4_return"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_S4_storeiri_io(self):
         behavior = self.insn_behavior["S4_storeiri_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_C2_cmpgtu(self):
         behavior = self.insn_behavior["C2_cmpgtu"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_C2_cmpgti(self):
         behavior = self.insn_behavior["C2_cmpgti"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_C2_xor(self):
         behavior = self.insn_behavior["C2_xor"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_C2_muxii(self):
         behavior = self.insn_behavior["C2_muxii"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_M2_mpyi(self):
         behavior = self.insn_behavior["M2_mpyi"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_sub(self):
         behavior = self.insn_behavior["A2_sub"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_paddfnew(self):
         behavior = self.insn_behavior["A2_paddfnew"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_psubfnew(self):
         behavior = self.insn_behavior["A2_psubfnew"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_paddit(self):
         behavior = self.insn_behavior["A2_paddit"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_addi(self):
         behavior = self.insn_behavior["A2_addi"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_abs(self):
         behavior = self.insn_behavior["A2_abs"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_nop(self):
         behavior = self.insn_behavior["A2_nop"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A4_ext(self):
         behavior = self.insn_behavior["A4_ext"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_tfr(self):
         behavior = self.insn_behavior["A2_tfr"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_tfrsi(self):
         behavior = self.insn_behavior["A2_tfrsi"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_combinew(self):
         behavior = self.insn_behavior["A2_combinew"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_combineii(self):
         behavior = self.insn_behavior["A2_combineii"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_A2_subri(self):
         behavior = self.insn_behavior["A2_subri"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_S2_lsr_i_vw(self):
         behavior = self.insn_behavior["S2_lsr_i_vw"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_S2_lsl_r_vw(self):
         behavior = self.insn_behavior["S2_lsl_r_vw"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_S2_cl0(self):
         behavior = self.insn_behavior["S2_cl0"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SA1_addi(self):
         behavior = self.insn_behavior["SA1_addi"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SA1_tfr(self):
         behavior = self.insn_behavior["SA1_tfr"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SA1_seti(self):
         behavior = self.insn_behavior["SA1_seti"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SA1_combinezr(self):
         behavior = self.insn_behavior["SA1_combinezr"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SA1_combine1i(self):
         behavior = self.insn_behavior["SA1_combine1i"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SL1_loadri_io(self):
         behavior = self.insn_behavior["SL1_loadri_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SL2_loadrh_io(self):
         behavior = self.insn_behavior["SL2_loadrh_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SL2_loadrd_sp(self):
         behavior = self.insn_behavior["SL2_loadrd_sp"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SL2_jumpr31_t(self):
         behavior = self.insn_behavior["SL2_jumpr31_t"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SS2_storeh_io(self):
         behavior = self.insn_behavior["SS2_storeh_io"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SS2_stored_sp(self):
         behavior = self.insn_behavior["SS2_stored_sp"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SS2_storew_sp(self):
         behavior = self.insn_behavior["SS2_storew_sp"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
     def test_SS2_allocframe(self):
         behavior = self.insn_behavior["SS2_allocframe"][0]
-        exc = self.compile_behavior(behavior)
-        self.assertIsNone(exc)
+        result = self.compile_behavior(behavior)
+        self.assertFalse(isinstance(result, Exception))
 
 
 class TestTransformerMeta(unittest.TestCase):
